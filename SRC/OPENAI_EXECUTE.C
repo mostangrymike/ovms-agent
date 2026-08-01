c45af866688cbbdbb849c168fd6323c262c573d6
MIKE <MIKE@vaxa.test.lcl>
2026-08-01 16:15:08 -0500

    M150D validate end-to-end create_file execution

--- SRC/OPENAI_EXECUTE.C
+++ SRC/OPENAI_EXECUTE.C
@@ -534,6 +534,7 @@ static int execute_save_ops_to(
 #include "openai_exec_m137_recovery.inc"
 #include "openai_exec_m137_validate.inc"
 #include "openai_m150b_validate.inc"
+#include "openai_m150d_validate.inc"
 void openai_plan_execute(agent_state *state)
 {
     openai_saved_operation *operations;
