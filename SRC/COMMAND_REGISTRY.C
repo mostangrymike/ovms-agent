f5254424eb7d5998d4ea2a4e59d84b65f76b4c66
mostangrymike <mikewommack86@gmail.com>
2026-08-18 13:51:53 -0500

    M262 register GitHub profile command

--- SRC/COMMAND_REGISTRY.C
+++ SRC/COMMAND_REGISTRY.C
@@ -6,6 +6,7 @@
 #define COMMAND_REGISTRY_MAX 256U
 
 void command_register_provider(void);
+void command_register_github(void);
 
 static command_entry command_registry[COMMAND_REGISTRY_MAX];
 static size_t command_registry_used;
@@ -64,6 +65,7 @@ void command_registry_initialize(void)
     command_register_symbol();
     command_register_agent();
     command_register_provider();
+    command_register_github();
 
     command_registry_ready = 1;
 }
