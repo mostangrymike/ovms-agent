d38baa5d3865491c68918ca9e915103ed237da13
mostangrymike <mikewommack86@gmail.com>
2026-08-19 08:24:50 -0500

    M267: migrate M215 test to LLM internal header

--- SRC/m215_repair_export_test.c
+++ SRC/m215_repair_export_test.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "openai_internal.h"
+#include "llm_internal.h"
 
 #define TEST_LOG "M215_REPAIR_EXPORT.LOG"
 #define TEST_OUT "M215_REPAIR_EXPORT.TXT"
