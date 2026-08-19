18869a13a16a5abcff386befa48b4f0a195249e0
mostangrymike <mikewommack86@gmail.com>
2026-08-19 11:08:52 -0500

    M267 migrate MCP network test to LLM internal header

--- SRC/M258_MCP_NET_TEST.C
+++ SRC/M258_MCP_NET_TEST.C
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "openai_internal.h"
+#include "llm_internal.h"
 
 int openai_net_allow_once(const char *domain);
 void openai_test_net_reset(void);
