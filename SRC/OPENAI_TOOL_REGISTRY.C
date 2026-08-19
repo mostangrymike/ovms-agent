c42de23520a6bf668429a8f4b9dba4bdc428b6cc
mostangrymike <mikewommack86@gmail.com>
2026-08-18 20:44:51 -0500

    Migrate tool registry to LLM internal header

--- SRC/OPENAI_TOOL_REGISTRY.C
+++ SRC/OPENAI_TOOL_REGISTRY.C
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "openai_internal.h"
+#include "llm_internal.h"
 #include "openai_tool_registry.h"
 
 static const openai_tool_descriptor tool_registry[] = {
