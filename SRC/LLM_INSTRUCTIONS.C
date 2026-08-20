c09ce614d5f7e5e6b5546ecf2d023fbd91ae0da4
mostangrymike <mikewommack86@gmail.com>
2026-08-20 13:43:26 -0500

    M267: neutralize instructions entry includes

--- SRC/LLM_INSTRUCTIONS.C
+++ SRC/LLM_INSTRUCTIONS.C
@@ -1,7 +1,7 @@
 /* M257 build entry: wrapper on first include, preserved M232 base on recursion. */
-#ifndef OPENAI_M257_INSTR_ENTRY
-#define OPENAI_M257_INSTR_ENTRY
-#include "openai_instructions_m257.c"
+#ifndef LLM_M257_INSTR_ENTRY
+#define LLM_M257_INSTR_ENTRY
+#include "LLM_INSTRUCTIONS_M257.C"
 #else
-#include "openai_instructions_base.c"
+#include "LLM_INSTRUCTIONS_BASE.C"
 #endif
