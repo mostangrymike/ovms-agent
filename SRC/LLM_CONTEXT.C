238978ebc1a8d12ab7e70e794b6bbe5699b19295
mostangrymike <mikewommack86@gmail.com>
2026-08-20 10:21:27 -0500

    M267 add neutral context implementation wrapper

---
+++ SRC/LLM_CONTEXT.C
@@ -0,0 +1,62 @@
+#include "LLM_CONTEXT.H"
+
+#define OPENAI_CONTEXT_MAX LLM_CONTEXT_MAX
+#define OPENAI_CONTEXT_META LLM_CONTEXT_META
+#define OPENAI_CONTEXT_HIST LLM_CONTEXT_HIST
+#define OPENAI_CONTEXT_TAIL LLM_CONTEXT_TAIL
+#define OPENAI_CONTEXT_PARENT LLM_CONTEXT_PARENT
+#define OPENAI_CONTEXT_RESULTS LLM_CONTEXT_RESULTS
+#define OPENAI_CONTEXT_PARENT_RES LLM_CONTEXT_PARENT_RES
+
+#define openai_context_append llm_context_append
+#define openai_context_tail llm_context_tail
+#define openai_context_evidence_text llm_context_evidence_text
+#define openai_context_build llm_context_build
+#define openai_context_current llm_context_current
+#define openai_show_context_current llm_show_context_current
+
+#include "LLM_CONTEXT_CORE.INC"
+
+#undef openai_show_context_current
+#undef openai_context_current
+#undef openai_context_build
+#undef openai_context_evidence_text
+#undef openai_context_tail
+#undef openai_context_append
+
+#undef OPENAI_CONTEXT_PARENT_RES
+#undef OPENAI_CONTEXT_RESULTS
+#undef OPENAI_CONTEXT_PARENT
+#undef OPENAI_CONTEXT_TAIL
+#undef OPENAI_CONTEXT_HIST
+#undef OPENAI_CONTEXT_META
+#undef OPENAI_CONTEXT_MAX
+
+/*
+ * M267 transition wrappers.  Keep existing broad consumers linkable until
+ * their large shared files can be migrated with a surgical edit path.
+ */
+int openai_context_evidence_text(const char *session,
+                                 char *output,
+                                 size_t output_size)
+{
+    return llm_context_evidence_text(session, output, output_size);
+}
+
+int openai_context_build(const char *goal,
+                         char *output,
+                         size_t output_size)
+{
+    return llm_context_build(goal, output, output_size);
+}
+
+int openai_context_current(char *output,
+                           size_t output_size)
+{
+    return llm_context_current(output, output_size);
+}
+
+void openai_show_context_current(void)
+{
+    llm_show_context_current();
+}
