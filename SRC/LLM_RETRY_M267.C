4b8ceb92ef1009217562b153868d906145d93c61
mostangrymike <mikewommack86@gmail.com>
2026-08-21 14:43:03 -0500

    M267: localize retry and parity compatibility boundaries

--- SRC/LLM_RETRY_M267.C
+++
@@ -1,6 +0,0 @@
-/* M267 neutral ABI wrapper for the mature retry implementation. */
-#define openai_agent_retry llm_agent_retry
-#define openai_agent_mode llm_agent_mode
-#include "LLM_RETRY.C"
-#undef openai_agent_mode
-#undef openai_agent_retry
