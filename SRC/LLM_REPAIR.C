c02cf7392390e4b7e5acffa84c25c58bb65bf537
mostangrymike <mikewommack86@gmail.com>
2026-08-21 16:45:30 -0500

    M267: retire repair plan compatibility export

--- SRC/LLM_REPAIR.C
+++ SRC/LLM_REPAIR.C
@@ -3,8 +3,3 @@
 #define openai_repair_plan llm_repair_plan
 #include "LLM_REPAIR_CORE.INC"
 #undef openai_repair_plan
-
-void openai_repair_plan(agent_state *state)
-{
-    llm_repair_plan(state);
-}
