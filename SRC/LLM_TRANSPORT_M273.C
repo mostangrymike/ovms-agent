37d9bf31f3b8933a1fc315a53281771d6a6442db
mostangrymike <mikewommack86@gmail.com>
2026-08-27 12:58:49 -0500

    Compile M276 rate-limit parser with transport

--- SRC/LLM_TRANSPORT_M273.C
+++ SRC/LLM_TRANSPORT_M273.C
@@ -1,5 +1,8 @@
 #include "LLM_USAGE.H"
 
+/* M276 bounded provider-rate-limit parsing is part of the transport object. */
+#include "LLM_RATE_LIMIT.C"
+
 #define perform_openai_request m273_raw_request
 #include "LLM_TRANSPORT.C"
 #undef perform_openai_request
